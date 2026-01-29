11842e7d67b6ccdf4dac44c235bd14960d3926f1
Saladin <dev@saladin.pro>
2026-01-29 22:51:55 +0530

    fixing audio latency in games

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -97,9 +97,14 @@
     pulse.enable = true;
     jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    extraConfig.pipewire."92-low-latency" = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.quantum" = 2048;
+        "default.clock.min-quantum" = 1024;
+        "default.clock.max-quantum" = 4096;
+      };
+    };
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
