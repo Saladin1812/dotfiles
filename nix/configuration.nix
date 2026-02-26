09c29d8320d51f6f217a9a5bd79d34eb0ef3ddf3
Saladin <dev@saladin.pro>
2026-02-26 19:24:00 +0530

    Enable Quad 9

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -49,6 +49,13 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
+  networking.networkmanager.dns = "none";
+  networking.nameservers = [
+    "9.9.9.9"
+    "149.112.112.112"
+    "2620:fe::fe"
+    "2620:fe::9"
+  ];
 
   # Set your time zone.
   time.timeZone = "Asia/Kolkata";
