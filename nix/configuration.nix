a890c59bde8be9a2d93068b660a8150c90a19de4
Saladin <dev@saladin.pro>
2026-06-12 07:06:44 +0530

    Use cloudflare DNS, fix magicDNS conflict

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -50,7 +50,18 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    dns = "systemd-resolved";
+    insertNameservers = [
+      "1.1.1.3"
+      "1.0.0.3"
+    ];
+  };
+  services.resolved = {
+    enable = true;
+    settings.Resolve.FallbackDNS = "1.1.1.3 1.0.0.3";
+  };
 
   # Set your time zone.
   time.timeZone = "Asia/Kolkata";
